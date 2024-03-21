f3f0541c61f0c346cef3681d0d5daa31b9e1e72c
Nikos Cocci <coccinikos@gmail.com>
2024-03-21 19:19:22 +0100

    created booksApi with rtk-query

---
+++ src/app/booksApiSlice.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+interface Book {
+  kind: string
+  id: string
+  etag: string
+  selfLink: string
+  volumeInfo: any
+  saleInfo: any
+  accessInfo: any
+  searchInfo: any
+}
+
+interface BooksApiResponse {
+  kind: string
+  totalItems: 1185
+  items: Book[]
+}
+
+const booksApiSlice = createApi({
+  baseQuery: fetchBaseQuery({baseUrl: "https://www.googleapis.com/books/v1/volumes"}),
+  reducerPath: 'booksApi',
+  endpoints: (builder) => ({
+    searchBooks: builder.query<BooksApiResponse, string>({
+      query: (searchTerm) => `?q=${searchTerm}`,
+    }),
+  }),
+})
+
+export const {useSearchBooksQuery} = booksApiSlice
+
+export default booksApiSlice
