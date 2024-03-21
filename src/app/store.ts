865cbfed1c22a0bf6f5def912d602d9b8bc20205
Nikos Cocci <coccinikos@gmail.com>
2024-03-21 19:20:32 +0100

    added search and booksApi reducers to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,21 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import searchSlice from "./searchSlice"
+import booksApiSlice from "./booksApiSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices()
+const rootReducer = combineSlices(searchSlice, booksApiSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
 export const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApiSlice.middleware),
 })
 
 // Infer the type of `store`
