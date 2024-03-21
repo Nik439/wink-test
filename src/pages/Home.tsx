42a53357ca17ed3ac7e9e6d93c0cb4d70566ff2d
Nikos Cocci <coccinikos@gmail.com>
2024-03-21 19:22:32 +0100

    added searchbar and basic books list to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+import { useSearchBooksQuery } from "../app/booksApiSlice";
+import { useAppSelector } from "../app/hooks";
+
 export default function HomePage () {
+  const {searchTerm, isWriting} = useAppSelector(state => state.search)
+  const { data, isError, isLoading, isSuccess } = 
+    useSearchBooksQuery(searchTerm, { skip: searchTerm==="" || isWriting })
+
   return (
     <div>
-      Home
+      <SearchBar/>
+      <Link to={'/test-id'}>link</Link>
+      <ul>
+        { isLoading ?
+          <p>Loading...</p>
+        : isError ?
+          <p>C'è stato un errore riprova più tardi</p>
+        : isSuccess &&
+          data.items.map(book=>(
+            <li key={book.id}>
+              <p>- {book.volumeInfo.title}</p>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
