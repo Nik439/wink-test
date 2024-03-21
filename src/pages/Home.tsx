e2472e8d70b845269fa18a774f0fb670aa7e49a8
Nikos Cocci <coccinikos@gmail.com>
2024-03-21 19:40:51 +0100

    added conditional rendering for when nothing is in the search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,26 +5,29 @@ import { useAppSelector } from "../app/hooks";
 
 export default function HomePage () {
   const {searchTerm, isWriting} = useAppSelector(state => state.search)
-  const { data, isError, isLoading, isSuccess } = 
+  const { data, isError, isLoading, isSuccess, isFetching } = 
     useSearchBooksQuery(searchTerm, { skip: searchTerm==="" || isWriting })
 
   return (
     <div>
       <SearchBar/>
       <Link to={'/test-id'}>link</Link>
-      <ul>
+      
         { isLoading ?
           <p>Loading...</p>
         : isError ?
-          <p>C'è stato un errore riprova più tardi</p>
-        : isSuccess &&
-          data.items.map(book=>(
-            <li key={book.id}>
-              <p>- {book.volumeInfo.title}</p>
-            </li>
-          ))
+          <p>C'è stato un errore, riprova più tardi</p>
+        : isSuccess ?
+          <ul>
+            {data.items.map(book=>(
+              <li key={book.id}>
+                <p>- {book.volumeInfo.title}</p>
+              </li>
+            ))}
+          </ul>
+        : 
+          <p>Scrivi nella barra di ricerca per trovare il libro che cerchi!</p>
         }
-      </ul>
     </div>
   )
 }
