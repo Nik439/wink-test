1f347fd55ff52c5fe2a3d9d8c4bfeac13ee18cc6
Nikos Cocci <coccinikos@gmail.com>
2024-03-23 16:55:39 +0100

    removed console logs

--- src/utils/loaders.ts
+++ src/utils/loaders.ts
@@ -7,15 +7,12 @@ export function searchLoader ({request}: {request: Request}): SearchLoaderRespon
   const searchParams = new URL(request.url).searchParams
   const search = searchParams.get('search') || ""
   const pageString = searchParams.get('p')
-  console.log(pageString);
   
   // if pageString is null return 0, if parseInt(pageString) is NaN return 0, otherwise return pageNumber
   let pageNumber = (pageString ? parseInt(pageString)-1 : 0) || 0
   
   if (pageNumber < 0)
     pageNumber = 0
-
-  console.log(pageNumber);
 
   return {
     searchQuery: search,
