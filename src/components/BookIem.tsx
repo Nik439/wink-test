1a124cb2530095860dae65fa60664d5e27845467
Nikos Cocci <coccinikos@gmail.com>
2024-03-23 10:32:51 +0100

    added responsive styling

--- src/components/BookIem.tsx
+++ src/components/BookIem.tsx
@@ -8,20 +8,21 @@ interface BookItemProps {
 export default function BookItem ({bookData}: BookItemProps) {
 
   return (
-    <li className="mb-7 xs:mb-10 max-w-200">
-      <p className="block xs:hidden font-semibold text-xl mb-1.5">{bookData.volumeInfo.title}</p>
+    <li className="mb-7 xs:mb-10 max-w-200 w-full">
+      <p className="line-clamp-2 xs:hidden font-semibold text-xl mb-1.5">{bookData.volumeInfo.title}</p>
       <div className="flex">
         {bookData.volumeInfo.imageLinks?.thumbnail ?
           <img className="w-20 xs:w-26" src={bookData.volumeInfo.imageLinks.thumbnail}/>
           :
-          <div className="h-36 w-26 bg-amber-100 border border-zinc-500 flex items-center">
+          <div className="h-28 xs:h-36 w-20 xs:w-26 bg-amber-100 border border-zinc-500 flex items-center">
             <p className="p-2 text-center text-sm text-zinc-600 leading-4">Immagine non disponibile</p>
           </div>
         }
-        <div className="ml-3 break-words w-[calc(100%-6.5rem-0.75rem)]">
-          <p className="hidden xs:block font-semibold text-xl mb-1.5">{bookData.volumeInfo.title}</p>
+        {/* width: 100% - thumbnailWidth - margin */}
+        <div className="ml-3 break-words w-[calc(100%-5rem-0.75rem)] xs:w-[calc(100%-6.5rem-0.75rem)]">
+          <p className="hidden xs:line-clamp-2 font-semibold text-xl mb-1.5">{bookData.volumeInfo.title}</p>
           {bookData.volumeInfo.authors &&
-            <p className="text-sm">
+            <p className="xs:text-sm">
               {bookData.volumeInfo.authors.map((author:string, index:number, authors: string[]) => (
               <Fragment key={bookData.id+author}>
                 {/* if it's not the last author add a comma at the end */}
