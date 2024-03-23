037628c1aa5557924643d8685af7f3a20a5a609f
Nikos Cocci <coccinikos@gmail.com>
2024-03-23 20:30:14 +0100

    added author links

--- src/components/BookIem.tsx
+++ src/components/BookIem.tsx
@@ -11,13 +11,13 @@ export default function BookItem ({bookData}: BookItemProps) {
   return (
     <li className="mb-7 xs:mb-10 max-w-200 w-full">
       <Link to={bookData.id}>
-        <p className="line-clamp-2 xs:hidden font-semibold text-xl mb-1.5">{bookData.volumeInfo.title}</p>
+        <p className="line-clamp-2 xs:hidden font-bold text-xl mb-1.5 hover:underline">{bookData.volumeInfo.title}</p>
       </Link>
       
       <div className="flex">
         <Link to={bookData.id}>
           {bookData.volumeInfo.imageLinks?.thumbnail ?
-            <img className="w-20 xs:w-26" src={bookData.volumeInfo.imageLinks.thumbnail}/>
+            <img className="w-20 xs:w-26  shadow-lg rounded-md" src={bookData.volumeInfo.imageLinks.thumbnail}/>
             :
             <div className="h-28 xs:h-36 w-20 xs:w-26 bg-amber-100 border border-zinc-500 flex items-center">
               <p className="p-2 text-center text-sm text-zinc-600 leading-4">Immagine non disponibile</p>
@@ -28,7 +28,7 @@ export default function BookItem ({bookData}: BookItemProps) {
         {/* width: 100% - thumbnailWidth - margin */}
         <div className="ml-3 break-words w-[calc(100%-5rem-0.75rem)] xs:w-[calc(100%-6.5rem-0.75rem)]">
           <Link to={bookData.id}>
-            <p className="hidden xs:line-clamp-2 font-semibold text-xl mb-1.5">{bookData.volumeInfo.title}</p>
+            <p className="hidden xs:line-clamp-2 font-bold text-xl mb-1.5 hover:underline">{bookData.volumeInfo.title}</p>
           </Link>
           
           {bookData.volumeInfo.authors &&
@@ -36,7 +36,7 @@ export default function BookItem ({bookData}: BookItemProps) {
               {bookData.volumeInfo.authors.map((author:string, index:number, authors: string[]) => (
               <Fragment key={bookData.id+author}>
                 {/* if it's not the last author add a comma at the end */}
-                <span>{author}</span>{index < authors.length-1 && ', '}
+                <Link to={`/?search=inauthor:"${author}"`} className="font-semibold hover:underline">{author}</Link>{index < authors.length-1 && ', '}
               </Fragment>
               ))}
               {bookData.volumeInfo.publishedDate &&
